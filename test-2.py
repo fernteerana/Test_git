28ec2d70387bdb41888c2c88fc6fd3046aec55a0
teerana boonloed <teerana.b@ku.th>
2022-06-08 10:21:52 +0700

    start

---
+++ test-2.py
@@ -0,0 +1,21 @@
+#def area(w,l):
+#   a = w*l
+#   return a
+
+# x = float(input("w :"))
+# y = float(input"l :"))
+
+# area = area(x,y)
+
+#print(area)
+
+from re import A
+
+
+def area(b,h):
+    a = 0.5*b*h
+    return A
+
+b = float(input("b :"))
+h = float(input("h :"))
+print ("area2(b,h)")
